a33242bd15746b6efe7166171e3473502a391193
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-24 01:15:07 +0800

    Added appropriate method for the word count

--- batch_5/print_number_of_entered_words.py
+++ batch_5/print_number_of_entered_words.py
@@ -1,3 +1,8 @@
 # Ask user for input
+statement = input("Enter a complete statemennt: ")
+
 # Count the number of words
+word_count = len(statement.split())
+
 # Print the result
+print("Word count:", word_count)
