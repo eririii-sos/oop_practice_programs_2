62dfb334c0f6f839357ba66c5db9c2941cf1db54
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-24 00:33:25 +0800

    Added string formatting

--- batch_5/print_input_in_all_caps.py
+++ batch_5/print_input_in_all_caps.py
@@ -1,3 +1,8 @@
 # Ask user for input
+full_name = input("Enter your full name: ")
+
 # Format all letters to all caps
+all_uppercase = full_name.upper()
+
 # Print the result
+print("Output:", all_uppercase)
