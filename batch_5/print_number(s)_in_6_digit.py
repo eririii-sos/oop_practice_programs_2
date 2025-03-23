c1390f7ad780cd9b5df5e19be428c260305cf5e3
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-24 00:25:05 +0800

    Added string formatting

--- batch_5/print_number(s)_in_6_digit.py
+++ batch_5/print_number(s)_in_6_digit.py
@@ -1,3 +1,8 @@
 # Ask user for input
+number = int(input("Enter a number (0-1000): "))
+
 # Format input into 6 digits
+formatted_number = f"{number:06d}"
+
 # Print result
+print("Output:", formatted_number)
