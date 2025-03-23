6f065c9c560baa345bc361f00938d0077297d44f
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-24 00:11:32 +0800

    Added string formatting

--- batch_5/print_input_without_spaces_in_beginning.py
+++ batch_5/print_input_without_spaces_in_beginning.py
@@ -1,3 +1,8 @@
 # Ask user for input
+full_name = input("Enter your full name beginning with space(s): ")
+
 # Remove the leading spaces
+formatted_name = full_name.lstrip()
+
 # Print result
+print("Output:", formatted_name)
