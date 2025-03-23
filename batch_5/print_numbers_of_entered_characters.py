c114a18d1dd1c01bf358d1ba3284c9e58c83148c
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-24 01:32:25 +0800

    Added appropriate method for the character count

--- batch_5/print_numbers_of_entered_characters.py
+++ batch_5/print_numbers_of_entered_characters.py
@@ -1,3 +1,8 @@
 # Ask user for input
-# Count number of characters
+characters = input("Enter any statement or word(s): ")
+
+# Count number of characters without including the spaces
+character_count = len(characters.replace(" ", ""))
+
 # Print the result
+print("Characters count:", character_count)
