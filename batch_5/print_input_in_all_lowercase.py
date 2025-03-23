cb92e0d46203d5c0952bb11d57937c012a49cfa0
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-24 00:40:21 +0800

    Added string format

--- batch_5/print_input_in_all_lowercase.py
+++ batch_5/print_input_in_all_lowercase.py
@@ -1,3 +1,8 @@
 # Ask user for input
+full_name = input("Enter your full name: ")
+
 # Format all letters to lowercase  
+all_lowercase = full_name.lower()
+
 # Print the result
+print("Output:", all_lowercase)
