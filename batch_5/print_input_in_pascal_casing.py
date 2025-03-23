774dfedcae24b965ae4d1e40c66f48531abfb332
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-24 01:45:29 +0800

    Added string formatting

--- batch_5/print_input_in_pascal_casing.py
+++ batch_5/print_input_in_pascal_casing.py
@@ -1,3 +1,8 @@
 # Ask user for input
+full_name = input("Enter your full name in random casing format: ")
+
 # Format input in pascal case 
+pascal_casing = full_name.title().replace(" ", "")
+
 # Print the result
+print("Output:", pascal_casing)
