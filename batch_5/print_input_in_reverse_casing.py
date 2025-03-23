c9390ce026c5942d6fe6eb054fbc30aa886a1d60
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-24 00:58:17 +0800

    Added string formatting

--- batch_5/print_input_in_reverse_casing.py
+++ batch_5/print_input_in_reverse_casing.py
@@ -1,3 +1,8 @@
 # Ask user for input
+full_name = input("Enter your full name: ")
+
 # Format input into reverse casing
+reversed_format = full_name.swapcase()
+
 # Print the result
+print("Output:", reversed_format)
