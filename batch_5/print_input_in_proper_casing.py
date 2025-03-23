7816d0bc519f67ce1b1a321da7298c50e0c1b377
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-24 00:48:40 +0800

    Added string formatting

--- batch_5/print_input_in_proper_casing.py
+++ batch_5/print_input_in_proper_casing.py
@@ -1,3 +1,8 @@
 # Ask user for input
+full_name = input("Enter full name with random casing format: ")
+
 # Format input into proper casing
+proper_casing = full_name.title()
+
 # Print the result
+print("Output:", proper_casing)
