3aa04e0c56da9d8432f04fe6f98a92839c934f94
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-03-24 01:56:15 +0800

    Added string formatting

--- batch_5/print_input_in_snake_casing.py
+++ batch_5/print_input_in_snake_casing.py
@@ -1,3 +1,8 @@
 # Ask user for input
+full_name = input("Enter your full name: ")
+
 # Format input into snake casing
+snake_casing = full_name.lower().replace(" ", "_")
+
 # Print the result
+print("Output:", snake_casing)
